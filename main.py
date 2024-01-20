f35357c2e10ced573724c784741f58e02a551ce0
JaYdEnTaNg123 <tangjayden999@gmail.com>
2024-01-20 11:59:59 +0800

    freddy rizzbear was here

--- main.py
+++ main.py
@@ -4,40 +4,62 @@ Q1:
 '''
 
 # TODO: Write your code here
-
+for i in range(7):
+    for j in range(i):
+        print(j, end=' ')
+    print()
 ##################################################
 '''
 Q2:
 '''
 
 # TODO: Write your code here
-
+for i in range(1,8):
+    for j in range(1,i):
+        print(j, end=' ')
+    print()
 ##################################################
 '''
 Q3:
 '''
 
 # TODO: Write your code here
-
+for j in range(9,0,-1):
+    for i in range(5):
+        print(j, end='')
+print()
 ##################################################
 '''
 Q4:
 '''
 
 # TODO: Write your code here
-
+for j in range(9,0,-1):
+    for i in range(j):
+        print(j, end='')
+print()
 ##################################################
 '''
 Q5:
 '''
 
 # TODO: Write your code here
-
+for j in range(1,6):
+    print('-'*(5-j+1),end='')
+    for i in range(j*2-1):
+        print(j*2-1, end='')
+    print()
 ##################################################
 '''
 Q6:
 '''
 
 # TODO: Write your code here
-
+for j in range(1,6):
+    print(' '*(5-j+1),end='')
+    for i in range(j):
+        print(i+1, end='')
+    for i in range(j-1,0,-1):
+        print(i, end='')    
+    print()
 ##################################################
